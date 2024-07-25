0692d0aa048035e065c7efa1f6f28e2ad6956734
lathiesh24 <lathumahi24@gmail.com>
2024-07-25 11:57:35 +0530

    recent

--- src/components/EventPage/AttendeesMarque.jsx
+++ src/components/EventPage/AttendeesMarque.jsx
@@ -14,19 +14,66 @@ const images = [
 
 const AttendeesMarquee = () => {
   return (
-    <div className="relative  overflow-hidden whitespace-nowrap select-none">
-      <motion.div className="flex scroll gap-24">
-        {images.concat(images).map((item, index) => (
-          <div key={index} className="flex-shrink-0 mx-2">
-            <img
-              src={item.src}
-              alt={item.alt}
-              className="w-32 h-32 object-contain"
-            />
-          </div>
-        ))}
-      </motion.div>
-    </div>
+    <>
+      {/* Regular div for larger screens */}
+      <div className="relative sm:flex hidden overflow-hidden whitespace-nowrap select-none">
+        <div className="flex scroll gap-24">
+          {images.concat(images).map((item, index) => (
+            <div key={index} className="flex-shrink-0 mx-2">
+              <img
+                src={item.src}
+                alt={item.alt}
+                className="w-32 h-32 object-contain"
+              />
+            </div>
+          ))}
+        </div>
+      </div>
+
+      {/* Motion div for mobile screens */}
+      <div className="flex sm:hidden overflow-hidden whitespace-nowrap select-none gap-12">
+        <motion.div
+          className="flex flex-shrink-0 gap-12"
+          initial={{ x: "0%" }}
+          animate={{ x: "-100%" }}
+          transition={{
+            duration: 10,
+            ease: "linear",
+            repeat: Infinity,
+          }}
+        >
+          {images.map((item, index) => (
+            <div key={index} className="mx-2">
+              <img
+                src={item.src}
+                alt={item.alt}
+                className="w-32 h-24 object-contain"
+              />
+            </div>
+          ))}
+        </motion.div>
+        <motion.div
+          className="flex flex-shrink-0 gap-12"
+          initial={{ x: "0%" }}
+          animate={{ x: "-100%" }}
+          transition={{
+            duration: 10,
+            ease: "linear",
+            repeat: Infinity,
+          }}
+        >
+          {images.map((item, index) => (
+            <div key={index} className="mx-2">
+              <img
+                src={item.src}
+                alt={item.alt}
+                className="w-32 h-24 object-contain"
+              />
+            </div>
+          ))}
+        </motion.div>
+      </div>
+    </>
   );
 };
 
