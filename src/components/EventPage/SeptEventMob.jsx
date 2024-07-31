12ce0edd56b283d7f172438fbd4dbd0bbf4c85e6
lathiesh24 <lathumahi24@gmail.com>
2024-07-31 06:46:39 +0530

    recent

--- src/components/EventPage/SeptEventMob.jsx
+++ src/components/EventPage/SeptEventMob.jsx
@@ -7,64 +7,54 @@ const SeptEventMob = ({
   isTransitioning,
 }) => {
   return (
-    <div className="flex flex-row shadow-[0_20px_50px_rgba(8,_112,_184,_0.7)] bg-bgBlue justify-around h-[10vh] fixed bottom-0 items-center w-full z-50">
-      {/* YInfinity image */}
-      <div className="cursor-pointer p-10" onClick={handleEventNavigation}>
-        <img src="/YInfinity.png" alt="YInfinity" className="h-[4vh] w-auto" />
-      </div>
-      {/* scrolling banner */}
-      {/* <div
-        className="w-8/12 flex flex-col items-center"
-        onClick={handleEventNavigation}
-      >
-        <div className="relative overflow-hidden h-10">
+    <>
+      {/* Fixed container for mobile */}
+      <div className="flex flex-col  bg-bgBlue justify-around fixed bottom-0 items-center w-full z-50">
+        {/* <div className="w-full flex flex-col items-center mt-2">
+          <div className="relative overflow-hidden h-full w-full">
+            <div
+              className={`flex ${
+                isTransitioning ? "transition-transform duration-2000" : ""
+              }`}
+              style={{
+                transform: `translateX(-${
+                  (currentSentence % sentences.length) * 100
+                }%)`,
+              }}
+            >
+              {sentences.concat(sentences).map((sentence, index) => {
+                let colorClass = "text-customBlue"; // Default to customBlue for all sentences
+                return (
+                  <div
+                    key={`${sentence}-${index}`} // Unique key for each element
+                    className={`w-full font-medium italic capitalize text-[14px] flex-shrink-0 flex justify-center items-center ${colorClass}`}
+                  >
+                    {sentence}
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+        </div> */}
+        <div className="flex flex-row justify-around items-center w-full p-4">
+          {/* YInfinity image */}
+          <div className="cursor-pointer " onClick={handleEventNavigation}>
+            <img
+              src="/YInfinity.png"
+              alt="YInfinity"
+              className="h-[4vh] w-auto"
+            />
+          </div>
+          {/* Join us button */}
           <div
-            className={`flex ${
-              isTransitioning ? "transition-transform duration-1000" : ""
-            }`}
-            style={{
-              transform: `translateX(-${
-                (currentSentence % sentences.length) * 100
-              }%)`,
-            }}
+            className={`border cursor-pointer bg-sky-600 text-white hover:text-customBlue shadow hover:bg-white py-1.5 px-8 rounded-3xl`}
+            onClick={handleEventNavigation}
           >
-            {sentences.concat(sentences).map((sentence, index) => {
-              let colorClass;
-              switch (index % sentences.length) {
-                case 0:
-                  colorClass =
-                    "bg-custom-gradient text-transparent bg-clip-text"; // Dark blue for the first sentence
-                  break;
-                case 1:
-                  colorClass = "text-customYellow"; // Bright yellow for the second sentence (CTA)
-                  break;
-                case 2:
-                  colorClass =
-                    "bg-custom-gradient text-transparent bg-clip-text";
-                  break;
-                default:
-                  colorClass = "text-gray-800";
-              }
-              return (
-                <div
-                  key={`${sentence}-${index}`} // Unique key for each element
-                  className={`w-full font-medium italic capitalize text-lg lg:text-2xl flex-shrink-0 flex justify-center items-center ${colorClass}`}
-                >
-                  {sentence}
-                </div>
-              );
-            })}
+            Join Us!
           </div>
         </div>
-      </div> */}
-      {/* Join us button */}
-      <div
-        className={`border cursor-pointer bg-sky-600 text-white hover:text-customBlue shadow hover:bg-white py-1.5 px-8 rounded-3xl`}
-        onClick={handleEventNavigation}
-      >
-        Join Us!
       </div>
-    </div>
+    </>
   );
 };
 
