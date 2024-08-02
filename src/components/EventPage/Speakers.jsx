a2728ceda477880d21effad260b623d0935b802a
lathiesh24 <lathumahi24@gmail.com>
2024-08-02 14:02:15 +0530

    recent

--- src/components/EventPage/Speakers.jsx
+++ src/components/EventPage/Speakers.jsx
@@ -5,7 +5,7 @@ const Speakers = () => {
   return (
     <>
       <div className="flex flex-col justify-center items-center sm:bg-blue-50 pb-12 sm:pb-24">
-        <div className="text-3xl sm:text-4xl text-customBlack sm:text-customBlue font-medium mt-16 sm:mt-12">
+        <div className="text-3xl sm:text-4xl text-customBlack sm:text-customBlue font-medium mt-28 sm:mt-12">
           <div className="grid">
             <div className="relative">
               <div className="absolute -top-8 -right-8 h-8 w-8">
@@ -15,7 +15,7 @@ const Speakers = () => {
             </div>
           </div>
         </div>
-        <div className="sm:pt-0 pt-12">
+        <div className="sm:pt-0 pt-20">
           <CardSpeakers />
         </div>
       </div>
