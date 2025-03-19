85989731ac9f7d7cd27d1dcbd39d725a93bd8d9e
Boopathi Hari <rboopathi@saleminfotech.com>
2025-03-19 23:18:06 +0530

    improve mobile responsiveness

--- src/components/Our Insights/ThirdPointData.jsx
+++ src/components/Our Insights/ThirdPointData.jsx
@@ -2,51 +2,53 @@ import React from "react";
 import data from "../../data/ThirdPoint.json";
 
 const ThirdPointData = () => {
-    return (
-        <div className="flex flex-col gap-4">
-            {Object.entries(data.partners).map(([key, value]) => (
-                <div key={key} className="">
-                    <h2 className="text-xl font-semibold mb-2">{key.replace(/_/g, " ")}</h2>
-                    <div className="grid grid-cols-2 gap-4">
-                        <div>
-                            <div>
-                                <h3 className="text-lg font-medium">Advantages:</h3>
-                                <ul className="pl-5 space-y-2">
-                                    {value.advantages.map((advantage, index) => (
-                                        <li key={index} className="custom-bullet">
-                                            {advantage}
-                                        </li>
-                                    ))}
-                                </ul>
-                            </div>
-                            <div className="mt-4">
-                                <h3 className="text-lg font-medium">Challenges:</h3>
-                                <ul className="pl-5 space-y-2">
-                                    {value.challenges.map((challenge, index) => (
-                                        <li key={index} className="custom-bullet">
-                                            {challenge}
-                                        </li>
-                                    ))}
-                                </ul>
-                            </div>
-                        </div>
+  return (
+    <div className="flex flex-col sm:gap-8 gap-4">
+      {Object.entries(data.partners).map(([key, value]) => (
+        <div key={key} className="">
+          <h2 className="sm:text-xl text-2xl font-semibold mb-2">
+            {key.replace(/_/g, " ")}
+          </h2>
+          <div className="grid sm:grid-cols-2  grid-cols-1 gap-4">
+            <div>
+              <div>
+                <h3 className="text-lg font-medium">Advantages:</h3>
+                <ul className="pl-5 space-y-2">
+                  {value.advantages.map((advantage, index) => (
+                    <li key={index} className="custom-bullet">
+                      {advantage}
+                    </li>
+                  ))}
+                </ul>
+              </div>
+              <div className="mt-4">
+                <h3 className="text-lg font-medium">Challenges:</h3>
+                <ul className="pl-5 space-y-2">
+                  {value.challenges.map((challenge, index) => (
+                    <li key={index} className="custom-bullet">
+                      {challenge}
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            </div>
 
-                        {/* Recommendations Section */}
-                        <div>
-                            <h3 className="text-lg font-medium">Recommendations:</h3>
-                            <ul className="pl-5 space-y-2">
-                                {value.recommendations.map((recommendation, index) => (
-                                    <li key={index} className="custom-bullet">
-                                        {recommendation}
-                                    </li>
-                                ))}
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-            ))}
+            {/* Recommendations Section */}
+            <div>
+              <h3 className="text-lg font-medium">Recommendations:</h3>
+              <ul className="pl-5 space-y-2">
+                {value.recommendations.map((recommendation, index) => (
+                  <li key={index} className="custom-bullet">
+                    {recommendation}
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </div>
         </div>
-    );
+      ))}
+    </div>
+  );
 };
 
 export default ThirdPointData;
--- src/components/Our Insights/WhitepaperOne.jsx
+++ src/components/Our Insights/WhitepaperOne.jsx
@@ -1,143 +1,186 @@
-import React from 'react'
-import PartnerLandScape from './PartnerLandScape'
-import ThirdPointData from './ThirdPointData'
-import Sidebar from './Sidebar'
+import React from "react";
+import PartnerLandScape from "./PartnerLandScape";
+import ThirdPointData from "./ThirdPointData";
+import Sidebar from "./Sidebar";
 
 const WhitepaperOne = () => {
-    return (
-        <div className='flex flex-col min-h-screen'>
-            <div class="relative w-full h-[400px]">
-                <img src="/bgwhitepaper.jpg" alt="Background" class="w-full h-full object-cover" />
-                <div class="absolute inset-0 flex items-center justify-center bg-black bg-opacity-50">
-                    <h1 class="text-white text-3xl font-bold text-center">
-                        Unlocking Non-Linear Growth through Strategic Alliances
-                    </h1>
-                </div>
-            </div>
-            <div className='flex flex-grow"'>
-                <div className='w-[250px] h-screen sticky top-[72px]'>
-                    <Sidebar />
-                </div>
-                <div className='px-8 flex-1 flex flex-col gap-8 py-10'>
-                    <div className='flex flex-col gap-8' id='executive-summary'>
-                        <div className='flex font-semibold text-3xl text-customBlack'>Executive Summary</div>
-                        <div className='flex flex-col gap-8 text-lg text-customGreyishBlack font-light'>
-                            <div>In today’s competitive tech landscape, it is imperative
-                                that emerging tech firms must scale quickly and
-                                secure enterprise clients in order to drive sustainable
-                                growth. Here, we explore how strategic alliances and
-                                channel partnerships can unlock such non-linear
-                                growth by leveraging ecosystem relationships.</div>
-                            <div>
-                                At The Yellow Network (TYN), we support emerging
-                                tech firms harness the power of partnerships through
-                                our Alliance-as-a-Service model. This document
-                                serves as a guide for tech founders to evaluate and
-                                implement channel-led growth strategies, ensuring
-                                long-term success.
-                            </div>
-                        </div>
-                    </div>
-                    <div className='flex flex-col gap-8' id='channel-growth'>
-                        <div className='flex font-semibold text-3xl text-customBlack'>Why Channel-Led Growth</div>
-                        <div className='flex flex-col gap-8 text-lg text-customGreyishBlack font-light'>
-                            <div>While direct sales yield good results in the short term,
-                                they fail to scale non-linearly. Channel-led growth,
-                                where third-party partners with an established market
-                                presence drive customer acquisition, accelerates
-                                enterprise penetration and market expansion, while
-                                also establishing and enhancing the credibility of
-                                emerging technologies.
-                            </div>
-                            <div>
-                                By partnering with key players, such as GSIs, SIs,
-                                hyperscalers, ISVs, and niche firms, emerging tech
-                                companies can unlock new market opportunities
-                                more quickly than through direct channels in the long
-                                term. However, building these alliances requires
-                                strategic planning, identifying the right partners, and
-                                aligning with ecosystem players.
-                            </div>
-                        </div>
-                    </div>
-                    <div className="flex flex-col gap-8" id='partner-landscape'>
-                        <div className='flex font-semibold text-3xl text-customBlack'>
-                           Partner Landscape
-                        </div>
-                        <PartnerLandScape />
-                    </div>
-                    <div className=" flex flex-col gap-8" id='who-to-work-with'>
-                        <div className='flex font-semibold text-3xl text-customBlack'>Who should you work with?</div>
-                        <div className='flex flex-col gap-8 text-lg text-customGreyishBlack font-light'>
-                            <div>Selecting the right partners is essential for scaling
-                                through channels. Each type of partner brings a
-                                unique blend of strengths, networks, and challenges.
-                                Understanding this along with their roles and how
-                                they fit into your growth strategy can help you unlock
-                                new markets, sales growth, and enhanced brand
-                                visibility.
-                            </div>
-                            <div>
-                                In the following section, we explore their specific
-                                advantages, challenges, and strategies for
-                                engagement for maximising growth and scale.
-                            </div>
-                        </div>
-
-                    </div>
-                    <div >
-                        <ThirdPointData />
-                    </div>
-                    <div className='flex flex-col gap-8' id='beyond-transactions'>
-                        <div className='text-3xl font-semibold text-customBlack'>Beyond Transactions</div>
-                        <div className='font-light  text-lg text-customGreyishBlack flex flex-col gap-8'>
-                            <div>
-                                For emerging tech firms, success and scale depends on
-                                delivering end-to-end solutions, and not just standalone
-                                products or point solutions. Partnering with multiple entities
-                                enables seamless integrations, services, and scalability,
-                                reducing friction and encouraging easier enterprise adoption.
-                            </div>
-                            <div>
-                                A multi-partner approach brings startups under a one-stop
-                                solution provider umbrella, reducing sales friction while
-                                increasing credibility. This strategy accelerates customer
-                                acquisition, and increases market access with long-term
-                                growth by leveraging collective strengths to deliver
-                                complete, enterprise-ready solutions.
-                            </div>
-                        </div>
-                    </div>
-                    <div className='flex flex-col gap-4' id='conclusion'>
-                        <div className='text-3xl font-semibold text-customBlack'>Conclusion and Summary</div>
-                        <div className='font-light text-lg text-customGreyishBlack'>
-                            In today’s hyper-competitive technology landscape, startups cannot rely on direct sales alone for sustained enterprise adoption and market penetration. The shift to SaaS, cloud-based business models, and ecosystem-led GTM strategies has made strategic alliances the most effective enabler for scalable, long-term growth.
-                        </div>
-                        <div className='flex flex-col gap-4'>
-                            <div className='text-lg'>Key points to consider:</div>
-                            <ul className="pl-5 space-y-4 text-customGreyishBlack text-lg">
-                                <li className="custom-bullet">Strategic Alliances Enable Exponential Growth</li>
-                                <li className="custom-bullet">Choosing the Right Partner is Crucial</li>
-                                <li className="custom-bullet">Executive buy-in and top-down alignment can make or break partnerships</li>
-                                <li className="custom-bullet">A Strong GTM Strategy is Essential</li>
-                                <li className="custom-bullet">Alliance Success Requires Continuous Engagement</li>
-                            </ul>
-                        </div>
-                        <div className='flex flex-col gap-4'>
-                            <div className='text-lg'>At The Yellow Network, we work with emerging tech firms to fast-track strategic alliances by:</div>
-                            <ul className="pl-5 space-y-4 text-customGreyishBlack text-lg">
-                                <li className="custom-bullet">Curating high-impact partnerships.</li>
-                                <li className="custom-bullet">Facilitating structured GTM execution for pipeline acceleration.</li>
-                                <li className="custom-bullet">Building scalable, long-term alliance models that maximise revenue potential.</li>
-                                <li className="custom-bullet">Enabling executive connects with partner organisations.</li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-            </div>
+  return (
+    <div className="flex flex-col min-h-screen">
+      <div class="relative w-full h-[400px]">
+        <img
+          src="/bgwhitepaper.jpg"
+          alt="Background"
+          class="w-full h-full object-cover"
+        />
+        <div class="absolute inset-0 flex items-center justify-center bg-black bg-opacity-50">
+          <h1 class="text-white text-3xl font-bold text-center">
+            Unlocking Non-Linear Growth through Strategic Alliances
+          </h1>
         </div>
-      
-    )
-}
+      </div>
+      <div className='flex flex-grow"'>
+        <div className="w-[250px] h-screen sticky top-[72px] hidden md:block">
+          <Sidebar />
+        </div>
+        <div className=" flex-1 flex flex-col gap-8 sm:py-10 sm:px-8 px-5 py-5">
+          <div className="flex flex-col sm:gap-8 gap-4" id="executive-summary">
+            <div className="flex font-semibold sm:text-3xl text-2xl text-customBlack">
+              Executive Summary
+            </div>
+            <div className="flex flex-col sm:gap-8 gap-4 sm:text-lg text-base text-customGreyishBlack font-light">
+              <div>
+                In today’s competitive tech landscape, it is imperative that
+                emerging tech firms must scale quickly and secure enterprise
+                clients in order to drive sustainable growth. Here, we explore
+                how strategic alliances and channel partnerships can unlock such
+                non-linear growth by leveraging ecosystem relationships.
+              </div>
+              <div>
+                At The Yellow Network (TYN), we support emerging tech firms
+                harness the power of partnerships through our
+                Alliance-as-a-Service model. This document serves as a guide for
+                tech founders to evaluate and implement channel-led growth
+                strategies, ensuring long-term success.
+              </div>
+            </div>
+          </div>
+          <div className="flex flex-col sm:gap-8 gap-4" id="channel-growth">
+            <div className="flex font-semibold sm:text-3xl text-2xl text-customBlack">
+              Why Channel-Led Growth
+            </div>
+            <div className="flex flex-col sm:gap-8 gap-4 sm:text-lg text-base text-customGreyishBlack font-light">
+              <div>
+                While direct sales yield good results in the short term, they
+                fail to scale non-linearly. Channel-led growth, where
+                third-party partners with an established market presence drive
+                customer acquisition, accelerates enterprise penetration and
+                market expansion, while also establishing and enhancing the
+                credibility of emerging technologies.
+              </div>
+              <div>
+                By partnering with key players, such as GSIs, SIs, hyperscalers,
+                ISVs, and niche firms, emerging tech companies can unlock new
+                market opportunities more quickly than through direct channels
+                in the long term. However, building these alliances requires
+                strategic planning, identifying the right partners, and aligning
+                with ecosystem players.
+              </div>
+            </div>
+          </div>
+          <div className="flex flex-col sm:gap-8 gap-4" id="partner-landscape">
+            <div className="flex font-semibold sm:text-3xl text-2xl text-customBlack">
+              Partner Landscape
+            </div>
+            <PartnerLandScape />
+          </div>
+          <div className=" flex flex-col sm:gap-8 gap-4" id="who-to-work-with">
+            <div className="flex font-semibold sm:text-3xl text-2xl text-customBlack">
+              Who should you work with?
+            </div>
+            <div className="flex flex-col sm:gap-8 gap-4 sm:text-lg text-base text-customGreyishBlack font-light">
+              <div>
+                Selecting the right partners is essential for scaling through
+                channels. Each type of partner brings a unique blend of
+                strengths, networks, and challenges. Understanding this along
+                with their roles and how they fit into your growth strategy can
+                help you unlock new markets, sales growth, and enhanced brand
+                visibility.
+              </div>
+              <div>
+                In the following section, we explore their specific advantages,
+                challenges, and strategies for engagement for maximising growth
+                and scale.
+              </div>
+            </div>
+          </div>
+          <div>
+            <ThirdPointData />
+          </div>
+          <div
+            className="flex flex-col sm:gap-8 gap-4"
+            id="beyond-transactions"
+          >
+            <div className="sm:text-3xl text-2xl font-semibold text-customBlack">
+              Beyond Transactions
+            </div>
+            <div className="font-light sm:text-lg text-base text-customGreyishBlack flex flex-col sm:gap-8 gap-4">
+              <div>
+                For emerging tech firms, success and scale depends on delivering
+                end-to-end solutions, and not just standalone products or point
+                solutions. Partnering with multiple entities enables seamless
+                integrations, services, and scalability, reducing friction and
+                encouraging easier enterprise adoption.
+              </div>
+              <div>
+                A multi-partner approach brings startups under a one-stop
+                solution provider umbrella, reducing sales friction while
+                increasing credibility. This strategy accelerates customer
+                acquisition, and increases market access with long-term growth
+                by leveraging collective strengths to deliver complete,
+                enterprise-ready solutions.
+              </div>
+            </div>
+          </div>
+          <div className="flex flex-col gap-4" id="conclusion">
+            <div className="sm:text-3xl text-2xl font-semibold text-customBlack">
+              Conclusion and Summary
+            </div>
+            <div className="font-light sm:text-lg text-base text-customGreyishBlack">
+              In today’s hyper-competitive technology landscape, startups cannot
+              rely on direct sales alone for sustained enterprise adoption and
+              market penetration. The shift to SaaS, cloud-based business
+              models, and ecosystem-led GTM strategies has made strategic
+              alliances the most effective enabler for scalable, long-term
+              growth.
+            </div>
+            <div className="flex flex-col gap-4 sm:text-lg text-base">
+              <div className="">Key points to consider:</div>
+              <ul className="pl-5 space-y-4 text-customGreyishBlack ">
+                <li className="custom-bullet">
+                  Strategic Alliances Enable Exponential Growth
+                </li>
+                <li className="custom-bullet">
+                  Choosing the Right Partner is Crucial
+                </li>
+                <li className="custom-bullet">
+                  Executive buy-in and top-down alignment can make or break
+                  partnerships
+                </li>
+                <li className="custom-bullet">
+                  A Strong GTM Strategy is Essential
+                </li>
+                <li className="custom-bullet">
+                  Alliance Success Requires Continuous Engagement
+                </li>
+              </ul>
+            </div>
+            <div className="flex flex-col gap-4 sm:text-lg text-base">
+              <div className="">
+                At The Yellow Network, we work with emerging tech firms to
+                fast-track strategic alliances by:
+              </div>
+              <ul className="pl-5 space-y-4 text-customGreyishBlack">
+                <li className="custom-bullet">
+                  Curating high-impact partnerships.
+                </li>
+                <li className="custom-bullet">
+                  Facilitating structured GTM execution for pipeline
+                  acceleration.
+                </li>
+                <li className="custom-bullet">
+                  Building scalable, long-term alliance models that maximise
+                  revenue potential.
+                </li>
+                <li className="custom-bullet">
+                  Enabling executive connects with partner organisations.
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default WhitepaperOne
+export default WhitepaperOne;
